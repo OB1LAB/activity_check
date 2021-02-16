f224ed6f330ca0b134264cff79bdd5df1762a91b
vadik <vadikverein45@gmail.com>
2021-02-16 19:16:26 +0500

    base for tests

--- test_all.py
+++ test_all.py
@@ -1,9 +1,14 @@
-import pytest
+import unittest
+from activity_check import check_activity
 
 
-class Test_Activity:
-    def __init__(self):
+class TestActivity(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
         pass
 
-    def test(self):
-        pass
+    def test_vanish(self):
+        with open('./test_vanish.txt.txt', 'r', encoding='UTF-8') as file:
+            log = file.read()
+        data = check_activity(log)
+        assert data == 'abc'
